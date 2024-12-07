86c54fc7dbdf9c5c5a3e0874331ff6b342f36c61
Wanda <wandawanda062@gmail.com>
2024-12-07 15:33:10 +0800

    wanda_2210010321

--- lib/pages/profil.dart
+++ lib/pages/profil.dart
@@ -12,9 +12,24 @@ class _ProfilState extends State<Profil> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('ini Profil'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://media1.tenor.com/m/YS05rtOt28IAAAAC/anthony-manchester-united.gif',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('NPM : 2210010321', style: TextStyle(fontSize: 20)),
+            Text('Nama : Wanda', style: TextStyle(fontSize: 20)),
+            Text('Kelas : 5N', style: TextStyle(fontSize: 20)),
+          ],
+        ),
       ),
     );
-    
   }
 }
